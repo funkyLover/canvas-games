22046ccdfceca62893c74b22e723f6a38e16146c
funkyHui <540886643@qq.com>
2014-04-17 16:35:51 +0800

    基本完成搬箱子动画

--- carry-box/game.js
+++ carry-box/game.js
@@ -8,19 +8,27 @@ $(document).ready(function () {
     var uiReset = $(".gameReset");
     var uiScore = $(".gameScore");
 
-    var playGame;//标志值,用于判定游戏开始与否
+    var playGame = false;//标志值,用于判定游戏开始与否
 
+
+    var left_boxes;//左侧箱子
+    var mid_boxes;//中间箱子
+    var right_boxes;;//右侧箱子
     var j = 106;//键值对应按键J,下面分别对应 K L A
     var k = 107;
     var l = 108;
     var a = 97;
     var colors = ["#CC3399", "#CCCC00","#FF0000","#FF3300", "#FFFF00", "#66FFCC", "51153204", "#3300FF","#00CC00"];
+    var check = false;//检测是否已有任何方位的箱子被选中
 
-    var boxes = new Array();//箱子
-
-    function roundRect(ctx, x, y, w, h, r) {
+    var timeoutId;//用于调用clearTimeout方法的参数
+    /*
+    * 画圆角矩形的方法
+    * */
+    function roundRect(ctx, x, y, w, h, r, style) {
         if (w < 2 * r) r = w / 2;
         if (h < 2 * r) r = h / 2;
+        ctx.strokeStyle = style;
         ctx.beginPath();
         ctx.moveTo(x+r, y);
         ctx.arcTo(x+w, y, x+w, y+h, r);
@@ -30,38 +38,72 @@ $(document).ready(function () {
         ctx.closePath();
     }
 
+    //箱子的构造函数
     var Box = function(x, y, color){
         this.x = x;
         this.y = y;
         this.color = color;
-        this.width = 120;
-        this.height = 40;
+        this.width = 119;
+        this.height = 39;
+        this.check = false;//是否被选中
+        this.linecolor = "rgb(0,0,0)";
     }
 
     function startGame(){
         uiScore.html("0");
         uiStats.show();
+        context.clearRect(0, 0, 360, 600);
+        left_boxes = new Array();//左侧箱子
+        mid_boxes = new Array();//中间箱子
+        right_boxes = new Array();//右侧箱子
         playGame = true;
         $(window).on("keypress", function(e) {
             var keyCode = e.which;
+            /*
+            * 这里有个问题,逻辑有点难搞懂,
+            * 当其中一个方位的箱子数组长度为0时不能正常进行选择
+            * */
+            if(left_boxes.length != 0 && right_boxes.length != 0 && mid_boxes.length != 0) {
+                check = left_boxes[0].check || right_boxes[0].check || mid_boxes[0].check ? true : false;
+            }
             if(keyCode == j) {
                 /*
-                * 选中左边的箱子或将箱子向左移
-                * */
-            } else if(keyCode == k) {
+                 * 选中左边的箱子或将箱子向左移
+                 * */
+                if(check){
+                    moveBox(j);
+                } else {
+                    selectBox(left_boxes);
+                }
+             } else if(keyCode == k) {
                 /*
                 * 选中中间的箱子或将箱子向中移
                 * */
-            } else if(keyCode == l) {
+                if(check){
+                    moveBox(k);
+                } else {
+                    selectBox(mid_boxes);
+                }
+             } else if(keyCode == l) {
                 /*
                 * 选中的右边的箱子或将箱子向右移
                 * */
-            } else if(keyCode == a) {
-                /*
-                * 让箱子立即下降一层
-                * */
+                if(check){
+                    moveBox(l);
+                } else {
+                    selectBox(right_boxes);
+                }
+             } else if(keyCode == a) {
+                clearTimeout(timeoutId);
+                animate();
             }
-            alert(keyCode);
+            if((left_boxes.length + 1)*40>600 || (mid_boxes.length + 1)*40>600 || (right_boxes.length + 1)*40>600) {
+                clearTimeout(timeoutId);
+                playGame = false;
+                uiStats.hide();
+                uiComplete.show();
+                $(window).unbind("keypress");
+            }
         });
         animate();
     }
@@ -84,40 +126,199 @@ $(document).ready(function () {
     }
 
     function animate(){
+        if((left_boxes.length + 1)*40>600 || (mid_boxes.length + 1)*40>600 || (right_boxes.length + 1)*40>600) {
+            clearTimeout(timeoutId);
+            playGame = false;
+            uiStats.hide();
+            uiComplete.show();
+            $(window).unbind("keypress");
+        }
+        //没次向3个方位的箱子数组添加箱子
+        pushBox(left_boxes, 0);
+        pushBox(mid_boxes, 120);
+        pushBox(right_boxes, 240);
+
+        //左方箱子向下移动
+        for(var i=0;i<left_boxes.length;i++){
+            left_boxes[i].y += 40;
+            context.save();
+            context.fillStyle = left_boxes[i].color;
+            roundRect(context, left_boxes[i].x, left_boxes[i].y, left_boxes[i].width, left_boxes[i].height, 10, left_boxes[i].linecolor);
+            context.stroke();
+            context.fill();
+            context.restore();
+        }
+        //中间箱子向下移动
+        for(var i=0;i<mid_boxes.length;i++){
+            mid_boxes[i].y += 40;
+            context.save();
+            context.fillStyle = mid_boxes[i].color;
+            roundRect(context, mid_boxes[i].x, mid_boxes[i].y, mid_boxes[i].width, mid_boxes[i].height, 10, mid_boxes[i].linecolor);
+            context.stroke();
+            context.fill();
+            context.restore();
+        }
+        //右方箱子向下移动
+        for(var i=0;i<right_boxes.length;i++){
+            right_boxes[i].y += 40;
+            context.save();
+            context.fillStyle = right_boxes[i].color;
+            roundRect(context, right_boxes[i].x, right_boxes[i].y, right_boxes[i].width, right_boxes[i].height, 10, right_boxes[i].linecolor);
+            context.stroke();
+            context.fill();
+            context.restore();
+        }
+        if(playGame) {
+            timeoutId = setTimeout(animate, 150);
+        }
+    }
+
+    function moveBox(keycode){
         /*
-        * 这里是游戏逻辑
+        * 移动箱子的方法,
+        * 写得很冗余,之后改进
+        * 先完成,再优化
         * */
+        if(keycode == j){
+            if(left_boxes[0].check){
+                left_boxes[0].check = false;
+                left_boxes[0].linecolor = "rgb(0,0,0)";
+                context.save();
+                context.fillStyle = left_boxes[0].color;
+                roundRect(context, left_boxes[0].x, left_boxes[0].y, left_boxes[0].width, left_boxes[0].height, 10, left_boxes[0].linecolor);
+                context.stroke();
+                context.fill();
+                context.restore();
+            } else if(mid_boxes[0].check) {
+                mid_boxes[0].check = false;
+                mid_boxes[0].linecolor = "rgb(0,0,0)";
+                context.clearRect(mid_boxes[0].x - 1, mid_boxes[0].y - 1, mid_boxes[0].width + 2, mid_boxes[0].height + 2);
+                var shiftBox = mid_boxes.shift();
+                shiftBox.y = left_boxes.length * 40;
+                shiftBox.x = 0;
+                left_boxes.unshift(shiftBox);
+                context.save();
+                context.fillStyle = left_boxes[0].color;
+                roundRect(context, left_boxes[0].x, left_boxes[0].y, left_boxes[0].width, left_boxes[0].height, 10, left_boxes[0].linecolor);
+                context.stroke();
+                context.fill();
+                context.restore();
+            } else if(right_boxes[0].check) {
+                right_boxes[0].check = false;
+                right_boxes[0].linecolor = "rgb(0,0,0)";
+                context.clearRect(right_boxes[0].x - 1, right_boxes[0].y - 1, right_boxes[0].width + 2, right_boxes[0].height + 2);
+                var shiftBox = right_boxes.shift();
+                shiftBox.y = left_boxes.length * 40;
+                shiftBox.x = 0;
+                left_boxes.unshift(shiftBox);
+                context.save();
+                context.fillStyle = left_boxes[0].color;
+                roundRect(context, left_boxes[0].x, left_boxes[0].y, left_boxes[0].width, left_boxes[0].height, 10, left_boxes[0].linecolor);
+                context.stroke();
+                context.fill();
+                context.restore();
+            }
+        }
+        if(keycode == k){
+            if(mid_boxes[0].check){
+                mid_boxes[0].check = false;
+                mid_boxes[0].linecolor = "rgb(0,0,0)";
+                context.save();
+                context.fillStyle = mid_boxes[0].color;
+                roundRect(context, mid_boxes[0].x, mid_boxes[0].y, mid_boxes[0].width, mid_boxes[0].height, 10, mid_boxes[0].linecolor);
+                context.stroke();
+                context.fill();
+                context.restore();
+            } else if(left_boxes[0].check) {
+                left_boxes[0].check = false;
+                left_boxes[0].linecolor = "rgb(0,0,0)";
+                context.clearRect(left_boxes[0].x - 1, left_boxes[0].y - 1, left_boxes[0].width + 2, left_boxes[0].height + 2);
+                var shiftBox = left_boxes.shift();
+                shiftBox.y = mid_boxes.length * 40;
+                shiftBox.x = 120;
+                mid_boxes.unshift(shiftBox);
+                context.save();
+                context.fillStyle = mid_boxes[0].color;
+                roundRect(context, mid_boxes[0].x, mid_boxes[0].y, mid_boxes[0].width, mid_boxes[0].height, 10, mid_boxes[0].linecolor);
+                context.stroke();
+                context.fill();
+                context.restore();
+            } else if(right_boxes[0].check) {
+                right_boxes[0].check = false;
+                right_boxes[0].linecolor = "rgb(0,0,0)";
+                context.clearRect(right_boxes[0].x - 1, right_boxes[0].y - 1, right_boxes[0].width + 2, right_boxes[0].height + 2);
+                var shiftBox = right_boxes.shift();
+                shiftBox.y = mid_boxes.length * 40;
+                shiftBox.x = 120;
+                mid_boxes.unshift(shiftBox);
+                context.save();
+                context.fillStyle = mid_boxes[0].color;
+                roundRect(context, mid_boxes[0].x, mid_boxes[0].y, mid_boxes[0].width, mid_boxes[0].height, 10, mid_boxes[0].linecolor);
+                context.stroke();
+                context.fill();
+                context.restore();
+            }
+        }
+        if(keycode == l){
+            if(right_boxes[0].check){
+                right_boxes[0].check = false;
+                right_boxes[0].linecolor = "rgb(0,0,0)";
+                context.save();
+                context.fillStyle = right_boxes[0].color;
+                roundRect(context, right_boxes[0].x, right_boxes[0].y, right_boxes[0].width, right_boxes[0].height, 10, right_boxes[0].linecolor);
+                context.stroke();
+                context.fill();
+                context.restore();
+            } else if(mid_boxes[0].check) {
+                mid_boxes[0].check = false;
+                mid_boxes[0].linecolor = "rgb(0,0,0)";
+                context.clearRect(mid_boxes[0].x - 1, mid_boxes[0].y - 1, mid_boxes[0].width + 2, mid_boxes[0].height + 2);
+                var shiftBox = mid_boxes.shift();
+                shiftBox.y = right_boxes.length * 40;
+                shiftBox.x = 240;
+                right_boxes.unshift(shiftBox);
+                context.save();
+                context.fillStyle = right_boxes[0].color;
+                roundRect(context, right_boxes[0].x, right_boxes[0].y, right_boxes[0].width, right_boxes[0].height, 10, right_boxes[0].linecolor);
+                context.stroke();
+                context.fill();
+                context.restore();
+            } else if(left_boxes[0].check) {
+                left_boxes[0].check = false;
+                left_boxes[0].linecolor = "rgb(0,0,0)";
+                context.clearRect(left_boxes[0].x - 1, left_boxes[0].y - 1, left_boxes[0].width + 2, left_boxes[0].height + 2);
+                var shiftBox = left_boxes.shift();
+                shiftBox.y = right_boxes.length * 40;
+                shiftBox.x = 240;
+                right_boxes.unshift(shiftBox);
+                context.save();
+                context.fillStyle = right_boxes[0].color;
+                roundRect(context, right_boxes[0].x, right_boxes[0].y, right_boxes[0].width, right_boxes[0].height, 10, right_boxes[0].linecolor);
+                context.stroke();
+                context.fill();
+                context.restore();
+            }
+        }
+    }
+
+    function selectBox(boxes){
+        boxes[0].check = boxes[0].check ? false : true;
+        boxes[0].linecolor = "rgb(255,255,255)";
+        context.save();
+        context.fillStyle = boxes[0].color;
+        roundRect(context, boxes[0].x, boxes[0].y, boxes[0].width, boxes[0].height, 10, boxes[0].linecolor);
+        context.stroke();
+        context.fill();
+        context.restore();
+    }
+    function pushBox(boxes, x){
         var num;
         var boxColor;
-
-        for(var i=0;i<boxes.length;i++){
-            boxes[i].y += 40;
-            context.save();
-            context.fillStyle = boxes[i].color;
-            roundRect(context, boxes[i].x, boxes[i].y, boxes[i].width, boxes[i].height, 10);
-            context.stroke();
-            context.fill();
-            context.restore();
-        }
-        for(var i=0;i<3;i++){
-            num = Math.floor(Math.random()*3);
-            boxColor = colors[num];
-            var box = new Box(120*i, 0, boxColor);
-            boxes.push(box);
-            context.save();
-            context.fillStyle = box.color;
-            context.strokeStyle = "rgb(0,0,0)";
-            roundRect(context, box.x, box.y, box.width, box.height, 10);
-            context.stroke();
-            context.fill();
-            context.restore();
-        }
-
-        if(playGame) {
-            setTimeout(animate, 1500);
-        }
-
-     }
+        num = Math.floor(Math.random()*3);
+        boxColor = colors[num];
+        var box = new Box(x, -40, boxColor);
+        boxes.push(box);
+    }
     init();
 
 });
